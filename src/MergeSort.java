cead471f6995e5ae4eb21f16858764f763bab722
Kevin Ho <keho98@gmail.com>
2014-04-15 09:57:02 -1000

    Slight refactor

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -9,34 +9,37 @@ public class MergeSort {
 	static void copy(int[] arr0, int[]arr1, int start, int finish) {
 		for(int i = start; i < finish; i++) arr1[i] = arr0[i];
 	}
+	static void merge(int[] arr, int start, int mid, int finish) {
+		int[] mergedArr = new int[arr.length];
+		int i0 = start;
+		int i1 = mid;
+		for(int i = start; i < finish; i++) {
+			if (i0 == mid) {
+				mergedArr[i] = arr[i1];
+				i1++;
+			}
+			else if (i1 == finish) {
+				mergedArr[i] = arr[i0];
+				i0++;
+			}
+			else if(arr[i0] < arr[i1]) {
+				mergedArr[i] = arr[i0];
+				i0++;
+			}
+			else {
+				mergedArr[i] = arr[i1];
+				i1++;
+			}
+		}
+		copy(mergedArr, arr, start, finish);	
+	}
 	static void mergeSort(int[] arr, int start, int finish) {
 		if(finish - start > 1) {
 			//String truncation automatically floors division
 			int mid = (start + finish)/2;
 			mergeSort(arr, start, mid);
 			mergeSort(arr, mid, finish);
-			int[] mergedArr = new int[arr.length];
-			int i0 = start;
-			int i1 = mid;
-			for(int i = start; i < finish; i++) {
-				if (i0 == mid) {
-					mergedArr[i] = arr[i1];
-					i1++;
-				}
-				else if (i1 == finish) {
-					mergedArr[i] = arr[i0];
-					i0++;
-				}
-				else if(arr[i0] < arr[i1]) {
-					mergedArr[i] = arr[i0];
-					i0++;
-				}
-				else {
-					mergedArr[i] = arr[i1];
-					i1++;
-				}
-			}
-			copy(mergedArr, arr, start, finish);
+			merge(arr, start,mid,finish);
 		}
 	}
 	public static void main(String[] args) {
