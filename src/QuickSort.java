25ffde44f2effe29a9873abf3da93280d76f07e4
Kevin Ho <keho98@gmail.com>
2014-04-20 00:37:31 -1000

    Implements quicksort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -2,29 +2,44 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class QuickSort {
+	static void swap(int[] arr, int i0, int i1) {
+		int tmp = arr[i0];
+		arr[i0] = arr[i1];
+		arr[i1] = tmp;
+	}
 	static void sort(int[] arr) {
 		quickSort(arr, 0, arr.length);
 	}
+	static int getPivot(int start, int finish) {
+		Random r = new Random();
+		return r.nextInt(finish - start) + start;
+	}
 	static void quickSort(int[] arr, int start, int finish) {
-		if(finish - start > 1) {
-			Random r = new Random();
-			int pivotIndex = r.nextInt(finish - start) + start;
-			partition(arr, pivotIndex, start, finish);
+		if(start < finish) {
+			int pivotIndex = getPivot(start,finish);
+			int newPivotIndex = partition(arr, pivotIndex, start, finish);
+			quickSort(arr, start, newPivotIndex - 1);
+			quickSort(arr, newPivotIndex + 1, finish);
 		}
 	}
-	static void partition(int[] arr, int pivotIndex, int start, int finish) {
-		int less = start;
-		int more = start;
-		for(int i = start; i < finish; i++){
-			if(arr[i] >= arr[pivotIndex]) {
+	static int partition(int[] arr, int pivotIndex, int start, int finish) {
+		swap(arr, pivotIndex, start);
+		int more;
+		int less = more = start + 1;
+		for(int i = start + 1; i < finish; i++){
+			if(arr[i] >= arr[start]) {
 				more++;
 			}
-			else if(arr[i] < arr[pivotIndex]){
+			else if(arr[i] < arr[start]){
 				//swap and increment both
+				swap(arr, more, less);
 				less++;
 				more++;
 			}
 		}
+		int newPivotIndex = less - 1;
+		swap(arr, start, newPivotIndex);
+		return newPivotIndex;
 	}
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
